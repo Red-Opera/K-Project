b9b6bd16942f2166b4c59dd513108bbdddb86663
chlwo200 <145303859+chlwo200@users.noreply.github.com>
2024-02-21 19:54:42 +0900

    건물 그림, Stage1 씬 추가, 씬 넘어가는거

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -1,7 +1,21 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class StageManager : MonoBehaviour
 {
-    
+    void Update()
+    {
+        // 스페이스바를 누르면 stage1로 이동
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            LoadStage1();
+        }
+    }
+
+    void LoadStage1()
+    {
+        // stage1 씬을 로드하는 코드
+        SceneManager.LoadScene("Stage1");
+    }
 }
