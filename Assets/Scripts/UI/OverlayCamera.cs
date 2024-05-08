7dee2b61a2e16c3a7dc0c2606ead825155a2181f
RedZoo <hyunsim01@naver.com>
2024-05-09 00:41:04 +0900

    플레이어 이미지 포맷 및 피벗 변경, 코스튬 UI 캐릭터 배치

--- Assets/Scripts/UI/OverlayCamera.cs
+++ Assets/Scripts/UI/OverlayCamera.cs
@@ -3,9 +3,20 @@ using UnityEngine.Rendering.Universal;
 
 public class OverlayCamera : MonoBehaviour
 {
+    private bool isAdd = false;
+
     // 자동으로 메인 카메라 스택에 넣음
-    void Start()
+    private void Start()
     {
+        AddCamera();
+    }
+
+    public void AddCamera()
+    {
+        if (isAdd)
+            return;
+
+        isAdd = true;
         Camera camera = Camera.main;
 
         camera.GetUniversalAdditionalCameraData().cameraStack.Add(GetComponent<Camera>());
