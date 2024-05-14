3a9dd79d2ba80abf3f9995c117e787e8a7116950
chlwo200 <145303859+chlwo200@users.noreply.github.com>
2024-05-14 13:14:00 +0900

    platform 설정중

---
+++ Assets/Scripts/Stage/Platform.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    bool playerCheck;
+    PlatformEffector2D platformObject;
+
+    void Start ()
+    {
+        playerCheck = false;
+        platformObject = GetComponent<PlatformEffector2D>();
+    }
+    /*void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S) && playerCheck)
+        {
+            platformObject = RotationOffset = 180f;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            platformObject = rotationalOffset = 0;
+        }
+        
+    }*/
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        playerCheck = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        playerCheck = false;
+    }
+
+}
+    
+    
