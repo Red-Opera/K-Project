bb2c06edf3e57be28dbf51f1b942f26fe7fb04a1
geoni-ara <lgeeckh@gmail.com>
2024-10-31 13:19:47 +0900

    222

---
+++ Assets/Scripts/Player/Weapon/VampireCount/VamCount_LongRangeAttack.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VamCount_LongRangeAttack : MyWeapon
+{
+    public override void InitSetting()
+    {
+        Weapon.AtkObject = Resources.Load<GameObject>("Prefab/Character/MagicSpear");
+        Weapon.AtkEffect = Resources.Load<GameObject>("Prefab/SkillEffect/blueplasma_vertical");
+        Weapon.pos = new Vector3(1.7f,0,0);
+        Weapon.coolTime = 0.75f;
+        Weapon.damage = GameManager.info.allPlayerState.damage;
+        Weapon.disapearTime = 5;    
+        Weapon.folloewTime = 0.8f;
+        Weapon.fowardSpeed = new Vector2(1.5f,0);
+        Weapon.animName = "LRAttack";
+    }
+}
